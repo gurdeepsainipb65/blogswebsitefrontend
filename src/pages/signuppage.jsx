6c8359cc0e7f37190488cf7f3903e25f06d8434d
gurdeepsainipb65 <gurdeepsainig2001@gmail.com>
2025-02-06 14:24:59 +0530

    updated

--- src/pages/signuppage.jsx
+++ src/pages/signuppage.jsx
@@ -1,5 +1,5 @@
-import axios, { Axios } from "axios";
-import React, { useState } from "react";
+import axios from "axios";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Signuppage = () => {
