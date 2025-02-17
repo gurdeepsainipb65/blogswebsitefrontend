85ece53011712d281e0ebe41762cedcd3981f3e6
gurdeepsainipb65 <gurdeepsainig2001@gmail.com>
2025-02-17 16:37:58 +0530

    update

--- src/BaseURL.jsx
+++ src/BaseURL.jsx
@@ -1,2 +1,2 @@
-// export const BaseURL = "https://blogswebsitebackend.onrender.com"
-export const BaseURL = "http://localhost:4000"
+export const BaseURL = "https://blogswebsitebackend.onrender.com"
+// export const BaseURL = "http://localhost:4000"
