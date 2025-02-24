e3b07fa55c37040ae16040170b6f6887b9b1ffd5
gurdeepsainipb65 <gurdeepsainig2001@gmail.com>
2025-02-24 12:32:29 +0530

    update

--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { is_authenticated, Logout } from "../authentication/authentication";
 
--- src/component/yourblog.jsx
+++ src/component/yourblog.jsx
@@ -2,8 +2,10 @@ import { useEffect, useState } from "react";
 import Blogscart from "./blogscart";
 import axios from "axios";
 import { BaseURL } from "../BaseURL";
+import Loader from "./loader";
 
 export default function Yourblog() {
+  const [loading, setloading] = useState(true);
   const [userblog, setuserblog] = useState([]);
   useEffect(() => {
     axios
@@ -13,19 +15,39 @@ export default function Yourblog() {
       .then((resp) => {
         console.log(resp);
         setuserblog(resp.data.blogs);
+        setloading(false)
+      })
+      .catch((error) => {
+        console.log(error);
+        setloading(false)
       });
   }, []);
 
-  return (
-      <div className="grid grid-cols-1 p-6 md:grid-cols-3 gap-8 container mx-auto">
-        {userblog.map((item, index) => (
-          <Blogscart
-            key={index}
-            image={item.image}
-            description={item.description}
-            name={item.name}
-          />
-        ))}
+
+  if (loading) {
+    return (
+      <div className="h-[72vh] flex justify-center items-center">
+        <Loader />
       </div>
+    );
+  }
+
+  return (
+    <div>
+      {userblog.length === 0 ? (
+        <p className="text-center text-xl text-gray-500">You Have No Blogs</p>
+      ) : (
+        <div className="grid grid-cols-1 p-6 md:grid-cols-3 gap-8 container mx-auto">
+          {userblog.map((item, index) => (
+            <Blogscart
+              key={index}
+              image={item.image}
+              description={item.description}
+              name={item.name}
+            />
+          ))}
+        </div>
+      )}
+    </div>
   );
 }
